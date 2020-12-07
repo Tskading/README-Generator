586973dcb5abb3237dac6610512f22e95622c81f
Tskading <Tskading@gmail.com>
2020-12-07 14:18:52 -0800

    Added screenshots and videolink to walkthrough/demo of project to README.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # README-Generator
-Command line README generator!
+Command line README generator utilizing npm inquirer. 
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Installation
+
+To run this package, navigate to my Github repository - [tskading](https://github.com/Tskading/README-Generator) - and clone the project.  From there, run 'npm i' in your intergrated terminal; this will install all dependent packages.  When complete, simply type 'node index.js' to run the program.  While running, you simply need to answer the prompts to get a custom README!  
+
+Notes:  Your generated file will be named 'myREADME'.  You can copy this file and place it into your current project, then rename the file to just 'README'.  Also, as the application exists at this time, you will overwrite the contents every time you run the prompts.  So, make sure you save your copy if you are pleased with the work you've done. 
+
+## Screenshots and Video Links
+
+[This link will take you to a video walkthough/demonstration of the applications functionality](https://drive.google.com/file/d/17PpOWkqyg_fhalbkaUAXb1vqgi5_9gFh/view)
+
+![Command Line Inquirer Prompts](/images/inquirerPrompts.png)
+
+![Sample README from Prompts](/images/sampleReadme.png)
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -75,7 +75,8 @@ function licenseInfo(license) {
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
-  } else if (license === 'GPL-3.0') {
+  } 
+  else if (license === 'GPL-3.0') {
     return `GNU LESSER GENERAL PUBLIC LICENSE
     Version 3, 29 June 2007
     
@@ -84,8 +85,7 @@ function licenseInfo(license) {
     Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
     
     This version of the GNU Lesser General Public License incorporates the terms and conditions of version 3 of the GNU General Public License, supplemented by the additional permissions listed below.`
-  } 
-  
+  }   
   else {return `Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
